2c9bd27baf9e6d8d4eecbfcf4eaf55bf285da2b8
atharvpathak84 <atharvspathak11@gmail.com>
2024-03-08 18:17:21 +0530

    EduSync Project WebDev

--- server.js
+++ server.js
@@ -330,6 +330,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-//Author : Manish Choudhary
-//Author : Manish Choudhary
